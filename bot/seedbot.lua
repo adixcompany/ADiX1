010ce77a566673d2391756abbf5a6db3857d3afd
AD Company <arashranjer026@gmail.com>
2016-11-16 18:17:05 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,18 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {226726601,206097989},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[ADiX Robot-Version1.0
+A best group manager for you!
+		
+ Created By ADiX Company 🌐
 
-https://github.com/SEEDTEAM/TeleSeed
+Special thanks to Seed Team,Github group and other...
 
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+Channel:
+@ADiXCo
 ]],
     help_text_realm = [[
 Realm Commands:
